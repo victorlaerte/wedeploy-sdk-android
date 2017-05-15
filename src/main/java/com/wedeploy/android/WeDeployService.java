90816d981dd9abd54ee0ab38084ab33ee3ce9caa
Silvio Santos <silvio.santos@liferay.com>
2017-05-15 16:39:30 -0300

    Support changing headers on requests
    fixes #4

--- src/main/java/com/wedeploy/android/WeDeployService.java
+++ src/main/java/com/wedeploy/android/WeDeployService.java
@@ -1,8 +1,13 @@
 package com.wedeploy.android;
 
 import com.wedeploy.android.auth.Authorization;
+import com.wedeploy.android.transport.MultiMap;
 import com.wedeploy.android.transport.Request;
 import com.wedeploy.android.transport.Response;
+import com.wedeploy.android.transport.SimpleMultiMap;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Silvio Santos
@@ -27,9 +32,21 @@ public class WeDeployService<T> {
 		return (T)this;
 	}
 
+	public T header(String name, String value) {
+		headers.put(name, value);
+
+		return (T)this;
+	}
+
 	protected Request.Builder newAuthenticatedRequestBuilder(String url) {
 		Request.Builder builder = new Request.Builder()
 			.url(url);
+
+		for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+			for (String value : entry.getValue()) {
+				builder.header(entry.getKey(), value);
+			}
+		}
 
 		Authorization authorization = getAuthorization();
 
@@ -41,11 +58,12 @@ public class WeDeployService<T> {
 	}
 
 	protected Call<Response> newCall(Request request) {
-		return new Call<>(request, weDeploy.getTransport(), weDeploy.getAsyncTransport(),
-			Response.class);
+		return new Call<>(
+			request, weDeploy.getTransport(), weDeploy.getAsyncTransport(), Response.class);
 	}
 
 	private Authorization authorization;
+	private MultiMap<String> headers = new SimpleMultiMap<>();
 	private final WeDeploy weDeploy;
 
 }
---
+++ src/test/java/com/wedeploy/android/WeDeployServiceTest.java
@@ -0,0 +1,32 @@
+package com.wedeploy.android;
+
+import com.wedeploy.android.transport.MultiMap;
+import com.wedeploy.android.transport.Request;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * @author Silvio Santos
+ */
+public class WeDeployServiceTest {
+
+	@Test
+	public void header_withSomeValues() {
+		WeDeployService service = new WeDeployService(new WeDeploy.Builder().build());
+		service.header("name1", "value1");
+		service.header("name2", "value2");
+		service.header("name3", "value3");
+		service.header("name3", "value4");
+
+		Request.Builder builder = service.newAuthenticatedRequestBuilder("http://someurl.com");
+		Request request = builder.build();
+
+		MultiMap<String> headers = request.getHeaders();
+		assertEquals("value1", headers.get("name1").get(0));
+		assertEquals("value2", headers.get("name2").get(0));
+		assertEquals("value3", headers.get("name3").get(0));
+		assertEquals("value4", headers.get("name3").get(1));
+	}
+
+}
