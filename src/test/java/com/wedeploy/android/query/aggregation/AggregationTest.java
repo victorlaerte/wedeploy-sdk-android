1e31d99f1e07085c8c945c66b19b4380ebd4f8d8
Andre Miranda <andre.miranda@liferay.com>
2018-08-23 17:46:53 -0300

    Introduce nested aggregations support

--- src/test/java/com/wedeploy/android/query/aggregation/AggregationTest.java
+++ src/test/java/com/wedeploy/android/query/aggregation/AggregationTest.java
@@ -105,6 +105,17 @@ public class AggregationTest {
 	}
 
 	@Test
+	public void testAggregation_nested() throws Exception {
+		JSONAssert.assertEquals(
+			"{\"field\":{\"name\":\"name\"," +
+				"\"aggregation\":[" +
+					"{\"field\":{\"name\":\"name\",\"operator\":\"avg\"}}]," +
+				"\"operator\":\"terms\"}}",
+			Aggregation.terms("name", "field").aggregate(
+				Aggregation.avg("name", "field")).bodyAsJson(), true);
+	}
+
+	@Test
 	public void testAggregation_range() throws Exception {
 		JSONAssert.assertEquals(
 			"{\"field\":{\"operator\":\"range\",\"name\":\"name\"," +
