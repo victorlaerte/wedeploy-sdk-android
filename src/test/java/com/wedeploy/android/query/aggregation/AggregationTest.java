1492c6c2f51d221a94a6bdd7246eb8042ddb2a9e
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-12-07 13:59:48 -0300

    Add test

--- src/test/java/com/wedeploy/android/query/aggregation/AggregationTest.java
+++ src/test/java/com/wedeploy/android/query/aggregation/AggregationTest.java
@@ -32,6 +32,7 @@ package com.wedeploy.android.query.aggregation;
 
 import com.wedeploy.android.query.SortOrder;
 import com.wedeploy.android.query.filter.BucketOrder;
+import com.wedeploy.android.query.filter.Filter;
 import com.wedeploy.android.query.filter.Range;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -83,6 +84,18 @@ public class AggregationTest {
 		JSONAssert.assertEquals(
 			"{\"field\":{\"operator\":\"extendedStats\",\"name\":\"name\"}}",
 			Aggregation.extendedStats("name", "field").bodyAsJson(), true);
+	}
+
+	@Test
+	public void testAggregation_filter() throws Exception {
+		Filter filter = Filter.gt("field", 0);
+
+		String body = Aggregation.filter("name", "field", filter).bodyAsJson();
+
+		JSONAssert.assertEquals(
+			"{\"field\":{\"name\":\"name\",\"value\":[{\"field\":{" +
+				"\"value\":0,\"operator\":\">\"}}],\"operator\":\"filter\"}}",
+			body, true);
 	}
 
 	@Test
